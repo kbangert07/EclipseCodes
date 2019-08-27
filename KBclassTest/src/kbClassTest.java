2739bc30935e2dfffae24553b6c4b34f2a4ae43b
Klaus Eppelheim <klaus.bangert@hotmail.de>
2019-08-27 06:52:19 +0200

    2nd issue finally

--- KBclassTest/src/kbClassTest.java
+++ KBclassTest/src/kbClassTest.java
@@ -3,7 +3,7 @@ public class kbClassTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("GITHUB Test hier mal);
+System.out.println("GITHUB Test hier mal wieder!");
 	}
 
 }
